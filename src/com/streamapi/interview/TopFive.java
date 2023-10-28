254b15303fa6a4e8b95015bfad4fec43e8d498c0
Umesh Tiwari <umesh.u.tiwari@gmail.com>
2023-10-28 21:28:22 +0530

    Create TopFive.java

---
+++ src/com/streamapi/interview/TopFive.java
@@ -0,0 +1,18 @@
+package com.java8.streams8;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class TopFive {
+
+	public static void main(String[] args) {
+		
+		List<Integer> inputList = Arrays.asList(1, 34, 56, 76, 54, 101, 99, 104, 23, 11, 99, 104);
+		
+		List<Integer> topFive = inputList.stream().distinct().sorted(Collections.reverseOrder()).limit(5).collect(Collectors.toList());
+		
+		System.out.println(topFive);
+	}
+}
